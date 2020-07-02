89ae77b62c9070141f93860c22746daf62c64620
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-02 14:04:11 -0500

    sidebar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StatisticsComponent } from './income-outcome/statistics/statistics.comp
 import { DetailComponent } from './income-outcome/detail/detail.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
     StatisticsComponent,
     DetailComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
