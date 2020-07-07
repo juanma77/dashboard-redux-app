a3c19d7ea5fb6d97885a1c6c446b97a4ec20bc8c
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-07 11:35:32 -0500

    loading optimization added

--- src/app/income-outcome/income-outcome.component.ts
+++ src/app/income-outcome/income-outcome.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IncomeOutcomeService } from '../services/income-outcome.service';
 import { IncomeOutcome } from '../models/income-outcome.model';
 import Swal from 'sweetalert2';
+import { AppState } from '../app.reducer';
+import { Store } from '@ngrx/store';
+import { IS_LOADING_ACTION, STOP_LOADING_ACTION } from '../shared/ui.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-income-outcome',
   templateUrl: './income-outcome.component.html',
   styleUrls: ['./income-outcome.component.css']
 })
-export class IncomeOutcomeComponent implements OnInit {
+export class IncomeOutcomeComponent implements OnInit, OnDestroy {
 
   public incomeForm: FormGroup; 
   
   public type: string = 'income'; 
 
-  constructor( private fb: FormBuilder, private incomeOutcomeService: IncomeOutcomeService ) { }
+  public loading: boolean = false; 
+
+  public loadingUnSubscription: Subscription; 
+
+  constructor( private fb: FormBuilder, private incomeOutcomeService: IncomeOutcomeService, private store:Store<AppState> ) { }
 
   ngOnInit() {
+
+    this.loadingUnSubscription = this.store.select('ui').subscribe( ui =>{
+
+      this.loading = ui.isLoading; 
+
+    } );
 
     this.incomeForm = this.fb.group({
 
@@ -28,6 +42,12 @@ export class IncomeOutcomeComponent implements OnInit {
 
   }
 
+  ngOnDestroy() {
+
+    this.loadingUnSubscription.unsubscribe(); 
+
+  }
+
   public saveIncome() {
 
     if( this.incomeForm.invalid ) {
@@ -35,6 +55,8 @@ export class IncomeOutcomeComponent implements OnInit {
       return; 
 
     }
+
+    this.store.dispatch( IS_LOADING_ACTION() );
 
     /*console.log( this.incomeForm.value );
     console.log( this.type );
@@ -49,11 +71,16 @@ export class IncomeOutcomeComponent implements OnInit {
       console.log( 'Income/Outcome added successfully!', res);
 
       this.incomeForm.reset();
+
+      this.store.dispatch( STOP_LOADING_ACTION() );
+
       Swal.fire('Ingreso/Egreso creado exitosamente', description, 'success'); 
 
     } ).catch( (err) =>{
 
       console.warn( 'An error happened while adding new Income/Outcome', err );
+
+      this.store.dispatch( STOP_LOADING_ACTION() );
 
       Swal.fire('Error al crear el Ingreso/Egreso', err.message, 'error');
 
